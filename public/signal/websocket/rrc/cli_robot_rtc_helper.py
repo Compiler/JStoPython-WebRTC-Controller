538ba9065428d49171ad7e20efe20f9d6ad1a5c0
compiler <lukerochedev@gmail.com>
2023-06-04 12:53:26 -0400

    showing buffer done, stable

--- public/signal/websocket/rrc/cli_robot_rtc_helper.py
+++ public/signal/websocket/rrc/cli_robot_rtc_helper.py
@@ -63,12 +63,17 @@ async def setup_callbacks(lc : RTCPeerConnection):
         
         
 
+async def rgb2depth(frame):
+    R = np.left_shift (np.bitwise_and(frame[:, :, 2], 0xf8).astype(np.uint16), 8)# 00000000 RRRRR000 -> RRRRR000 00000000
+    G = np.left_shift (np.bitwise_and(frame[:, :, 1], 0xfc).astype(np.uint16), 3)# 00000000 GGGGGG00 -> 00000GGG GGG00000
+    B = np.right_shift(np.bitwise_and(frame[:, :, 0], 0xf8).astype(np.uint16), 3)
+    I = np.bitwise_or(R, G, B)
+    return I
 
         
 
-
-
-
+async def depth2rgb(depth):
+    return cv2.applyColorMap(np.sqrt(depth).astype(np.uint8), cv2.COLORMAP_HSV)
 
 
 
@@ -102,6 +107,7 @@ async def set_offer(offer):
     offer_wrapped = RTCSessionDescription(offer['sdp'], offer['type'])
     await lc.setRemoteDescription(offer_wrapped)
     
+import open3d
 async def run():
     import cv2
     global dc
@@ -110,29 +116,24 @@ async def run():
     while(cur_frame is None): await asyncio.sleep(2)
     count = 0;
     while True:
-        #try:
         f = await cur_frame.recv()
-        #print("IN loop")
         if not f:
             await asyncio.sleep(2)
         else:
-            #print("Running")
             if count == 15 :
                 if not dc == None:
                     print("Dc send msg to robo xD")
                     dc.send('keydown:KeyA')
                     dc.send('keyup:KeyA')
             count = (count + 1) % 100
-            #print(i.shape)
+            
+            
             np_frame = np.array(f.to_image())
-            cv2.imshow('robo_view', np_frame)
+            d_frame = await rgb2depth(np_frame)
+            f_frame = await depth2rgb(d_frame)
+            cv2.imshow('robo_view', f_frame)
             cv2.waitKey(1);
-            #gray = np.ones((480, 640,  3), np.uint8) * 128
-            #cv2.imshow('v', gray); cv2.waitKey(0);
-            #cv2.imshow('view', i)
-            #cv2.waitKey(1);
-        # except Exception as e:
-        #     print("error", e)
+
     print("Terminated")
     # while True:
     #     try:
